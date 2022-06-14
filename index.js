7b95ffcdcda72f605c709ff0146995a1b3bd0fd4
Thomas <chearful@chearfulcraft.org>
2022-06-14 23:58:41 +0100

    Node parsing

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const cheario = require("cheerio");
 const fs = require("fs");
+const sqlstring = require("sqlstring");
+var $ = cheario.load(fs.readFileSync("./planet.osm"), {xmlMode: true});
 
-var $ = cheerio.load(fs.readFileSync("./planet.osm"), {xmlMode: true});
+$("node").each(function() {
+    let node_id = sqlstring.escape($(this).attr("id"));
+    console.log("INSERT INTO `node` SET id = " + node_id + ", position = POINT(" + sqlstring.escape($(this).attr("lon")) + "," + sqlstring.escape($(this).attr("lat")) + ");");
+    $(this).children("tag").each(function() {
+        console.log("INSERT INTO `node_tag` SET node_id = " + node_id + ", `name` = " + sqlstring.escape($(this).attr("k")) + ", `value` = " + sqlstring.escape($(this).attr("v")) + ";");
+    });
+});
+
